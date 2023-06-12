e4da8f35acd230869d5806430e2ad3f75475c64f
suneralp <89111414+suneralp@users.noreply.github.com>
2023-06-12 12:27:36 +0200

    Add files via upload

---
+++ CreateAccountFinal.ps1
@@ -0,0 +1,54 @@
+﻿function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+
+Import-Module activedirectory
+$logfile = "C:\Users\alp\PowerShell\ADUSERCREATION\LOGS\" + (Get-Date -f yyyy-MM-dd-hh-mm) + ".log"
+$UPN = "mylab.local" 
+$ADUsers = Import-Csv C:\Users\alp\PowerShell\ADUSERCREATION\NewAccounts_20230517_041621AM.csv  -Delimiter ","
+
+ 
+
+foreach ($User in $ADUsers)
+{
+            $SamAccountName = $User.employeeID
+            $EmployeeNumber = $User.employeeID
+            $EmployeeID = $user.employeeID
+            $Firstname = $User.firstname
+            $Lastname = $User.lastname
+            $Division = $User.Division
+            $Company = $User.Company
+            $SuperVisorID = $User.SuperVisorID
+ 
+    if (Get-ADUser -F {SamAccountName -eq $EmployeeID})
+        {
+          Write-Warning "A user account with username $SamAccountName already exists in Active Directory." 
+          Write-output "A user account with username $SamAccountName already exists in Active Directory." | Out-File -FilePath $logfile  -append
+        }
+    else
+        {
+            New-ADUser `
+                -SamAccountName $SamAccountName `
+                -UserPrincipalName "$EmployeeID@$UPN" `
+                -Name "$Firstname $Lastname" `
+                -EmployeeNumber $EmployeeNumber `
+                -GivenName $Firstname `
+                -Surname $Lastname `
+                -AccountPassword (ConvertTo-secureString "PassWord123" -AsPlainText -Force) -ChangePasswordAtLogon $True `
+                -EmployeeID $EmployeeID `
+                -Enabled $True `
+                -Path "OU=MOT,OU=USER,DC=MYLAB,DC=LOCAL" `
+                -DisplayName "$firstname $lastname" `
+                -Division $Division `
+                -Company $Company `
+                -Manager $SuperVisorID
+                $X = Get-TimeStamp
+                write-output $X  | Out-File -FilePath $logfile  -append
+                Write-Warning "The user account $SamAccountName is created logfile generated"
+                Write-output "The user account $SamAccountName is created."|Out-File -FilePath $logfile  -append
+                get-ADUser -Identity $SamAccountName -Property *|Select employeeID,employeenumber,distinguishedname,samAccountName,UserPrincipalName,GivenName,Surname,DisplayName,Manager,Enabled,Company,Division|Out-File -FilePath $logfile  -append
+
+        }
+}
+
+             
---
+++ DisableAccounts.ps1
@@ -0,0 +1,36 @@
+﻿ function Get-TimeStamp {
+    
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+    
+}
+
+Import-Module activedirectory
+
+$logfile = "c:\scripts\" + (Get-Date -f yyyy-MM-dd-hh-mm) + ".log"
+
+
+$Users = Import-csv C:\Users\alp\PowerShell\ADUSERCREATION\disableusers.txt
+
+foreach ($User in $Users) {
+  
+   $Username       = $User.SamAccountName
+
+      if (Get-ADUser -F {SamAccountName -eq $Username}) {
+         
+         Write-Warning "An account with username $Username is disabled."
+         Set-ADUser -Identity $Username -Enabled $false
+        
+         $X = Get-TimeStamp
+         write-output $X  | Out-File -FilePath $logfile  -append  
+                
+         get-ADUser -Identity $username |Select-Object -Property samaccountname,enabled|Out-File -FilePath $logfile  -append
+    }
+    else {
+        
+         Write-Host "A user account with username $Username does not exist in Active Directory." -ForegroundColor Cyan
+        }     
+
+     
+   
+    } 
+  
